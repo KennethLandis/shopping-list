a612e7c20a2d15e87271ebef68d8efd16e652188
Kenneth Landis <Eightfletcher@msn.com>
2021-03-04 11:19:53 -0600

    added function to check/uncheck items on shopping list

--- index.js
+++ index.js
@@ -5,14 +5,22 @@
 
 //Function for adding new items to the shopping list//
 
+
 function handleNewItems() {
     $('#js-shopping-list-form').submit(event => {
         event.preventDefault();
         const addedItem = $(this).find('#shopping-list-entry').val();
-        console.log(addedItem);
 
         $('.shopping-list').append('<li> <span class="shopping-item">' +addedItem+ '</span> <button class="shopping-item-toggle"> <span class="button-label">check</span> </button> <button class="shopping-item-delete"> <span class="button-label">delete</span>');
     });
 };
 
+function handleItemCheck() {
+    $('.shopping-item-toggle').click(function() {
+        $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked')
+    });
+
+};
+
 $(handleNewItems);
+$(handleItemCheck);
